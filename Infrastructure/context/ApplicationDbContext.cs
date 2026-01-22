02af4ed686d9750696c22eee461d67b893fde9e4
ayman <aymanehab2002@gmail.com>
2026-01-23 01:13:24 +0200

    feat(domain): Add custom User entity and initial migration
    - Addea `User` entity extending `IdentityUser` with properties: `Name`, `PhotoUrl`, and `JoinedAt`.
    refactor(Infrastructure)
    - Update `ApplicationDbContext` to use `IdentityDbContext<User>` and added necessary configurations.
    
    - Add initial migration to create Identity tables and configure relationships.

--- Infrastructure/context/ApplicationDbContext.cs
+++ Infrastructure/context/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Domain.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.context;
 
-public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<User>(options)
 {
 
 }
