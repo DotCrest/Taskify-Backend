4c85d4d841a88ed32fd51c4b5f7ce5e61a209f8c
BassimEhab <bassimstudy@gmail.com>
2026-01-22 22:42:13 +0200

    feat: setup database context and connection string

---
+++ Infrastructure/context/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.context;
+
+public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options)
+{
+
+}
