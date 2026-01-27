309797e8c8d2685eaec536f3061b2936978c6e68
BassimEhab <bassimstudy@gmail.com>
2026-01-28 01:48:06 +0200

    Refactor(Infrastructure): Introduce custom names for Identity tables, reduce the size of UserId and replace inital migration.
    Replaced initial migration with a new schema using custom table names for Identity and introduced new entities (Category, Comment, Invitation, Quest, Workspace, Space, Group, Tag, SpaceMember, WorkspaceMember, UserQuest).

--- Infrastructure/context/ApplicationDbContext.cs
+++ Infrastructure/context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entites;
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,5 +7,32 @@ namespace Infrastructure.context;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<User>(options)
 {
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Invitation> Invitations { get; set; }
+    public DbSet<Quest> Quests { get; set; }
+    public DbSet<Workspace> Workspaces { get; set; }
+    public DbSet<Space> Spaces { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<SpaceMember> SpaceMembers { get; set; }
+    public DbSet<WorkspaceMember> WorkspaceMembers { get; set; }
+
+    override protected void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<User>().ToTable("UsersAccount");
+        builder.Entity<User>().Property(u => u.Id).HasMaxLength(128);
+        builder.Entity<IdentityRole>().ToTable("Roles");
+        builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+        builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+
+
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 
 }
