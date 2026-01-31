9f814a4c88c52794aeb48e62b4f4f14f1e06e6ea
BassimEhab <bassimstudy@gmail.com>
2026-01-31 22:36:14 +0200

    feat(Domain,Infrastructure,WebApi): Add generic repository and unit of work patterns
    Introduced IGenericRepository<T> and IUnitOfWork<T> interfaces for abstracting data access and transaction management. Implemented their concrete classes Registered these services with dependency injection in Program.cs.

---
+++ Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Contracts;
+using Infrastructure.context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repository
+{
+    public class GenericRepository<T>(ApplicationDbContext context) : IGenericRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public Task<T?> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.FirstOrDefaultAsync(predicate);
+        }
+        public async Task<IEnumerable<T>> FindAll(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+
+
+    }
+}
