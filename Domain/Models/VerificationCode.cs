50107139228615757e7ae5056b8815f8a9ac2a9c
BassimEhab <bassimstudy@gmail.com>
2026-02-04 15:54:04 +0200

    refactor(Domain,Infrastructure) : Add email column to 'verification code' tabel.

--- Domain/Models/VerificationCode.cs
+++ Domain/Models/VerificationCode.cs
@@ -4,6 +4,7 @@ public class VerificationCode
 {
     public int Id { get; set; }
     public string Code { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
     public bool IsActive => DateTime.Now < CreatedAt.AddMinutes(10);
 }
