d3ad3aca2d53405667835b0138c3b73a2d1e581d
Bassem Ehab <121682008+BassimEhab@users.noreply.github.com>
2026-02-04 16:15:26 +0200

    Merge pull request #6 from feature/database
    refactor(Domain,Infrastructure) : Add email column to 'verification code' tabel.

---
+++ Infrastructure/Migrations/20260204133028_AddEmailColumnToVerficationCodeTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailColumnToVerficationCodeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "VerificationCodes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "VerificationCodes");
+        }
+    }
+}
