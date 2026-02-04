5f5daf8a7180fbcd85768da5ba87cd200c4d93f9
BassimEhab <bassimstudy@gmail.com>
2026-02-04 23:38:26 +0200

    refactor(Application(: Refactor Error class to record; update AuthErrors usage
    Refactored Error class to an immutable record type, removing the factory method and unused usings. Updated AuthErrors to instantiate Error directly with the new constructor syntax for improved clarity and immutability.

--- Application/Shared/Errors/AuthErrors.cs
+++ Application/Shared/Errors/AuthErrors.cs
@@ -1,21 +1,16 @@
 ﻿using Application.Common.Errors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Shared.Errors
 {
     public static class AuthErrors
     {
         public static readonly Error InvalidCredentials =
-            Error.ErrorFactory("Auth.InvalidCredentials", "Invalid Email Or Password");
+            new Error("Auth.InvalidCredentials", "Invalid Email Or Password");
         public static readonly Error UserLockedOut =
-            Error.ErrorFactory("Auth.UserLockedOut", "Account Is Locked due to many failed attempts");
+            new Error("Auth.UserLockedOut", "Account Is Locked due to many failed attempts");
         public static readonly Error EmailAlreadyExsists =
-            Error.ErrorFactory("Auth.EmailAlreadyExsist", "User with this email  already exsisit");
+            new Error("Auth.EmailAlreadyExsist", "User with this email  already exsisit");
         public static readonly Error UsernameAlreadyExsists =
-            Error.ErrorFactory("Auth.UsernameAlreadyExsist", "User with this username already exists");
+            new Error("Auth.UsernameAlreadyExsist", "User with this username already exists");
     }
 }
