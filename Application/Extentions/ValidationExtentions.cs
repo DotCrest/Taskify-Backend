96b736c266793aa8f85ce69278e40c37a25caafa
ayman <aymanehab2002@gmail.com>
2026-02-09 01:44:15 +0200

    feat(application) : adding extention method to convert from validation result errors to Error class and pass them to Result<t>.failure()

---
+++ Application/Extentions/ValidationExtentions.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Errors;
+using Application.Shared;
+
+namespace Application.Extentions
+{
+    public static class ValidationExtentions
+    {
+        public static Result<T> ToFailure<T>(this FluentValidation.Results.ValidationResult validationResult)
+        {
+            var errors = validationResult.Errors
+                .Select(e => new Error(e.PropertyName, e.ErrorMessage))
+                .ToList();
+            return Result<T>.Failure(errors);
+
+
+        }
+    }
+}
