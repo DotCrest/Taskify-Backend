d220d864bc9da54017eaef9f34d1349ccd47fcfb
ayman <aymanehab2002@gmail.com>
2026-02-09 03:18:07 +0200

    refactor(webapi) `builder.Services.AddValidatorsFromAssembly` method to scan the assembly of `RegisterDtoValidator` instead of the `Program` class,

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Application.ServiceAbstractions;
 using Application.Services;
+using Application.Validators;
 using Domain.Contracts;
 using Domain.Models;
 using Domain.Options;
@@ -35,7 +36,7 @@ builder.Services.AddScoped<PasswordHasher<User>>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.Configure<EmailOptions>(builder.Configuration.GetSection("email-config"));
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+builder.Services.AddValidatorsFromAssembly(typeof(RegisterDtoValidator).Assembly, includeInternalTypes: true);
 builder.Services.AddIdentity<User, IdentityRole>(opt =>
 {
     opt.Password.RequireDigit = true;
