5902659e1a417b58fec9d95a132aff0dd2cc69be
BassimEhab <bassimstudy@gmail.com>
2026-02-09 22:58:56 +0200

    refactor(WebApi): Refactor controller to inherit from BaseApiController
    feat(WebApi):  Document endpoint responses
    
    Refactored controller to inherit from BaseApiController for shared API functionality. Added error handling imports and standardized error responses using HandleFailure. Documented endpoint responses with ProducesResponseType attributes.

--- WebApi/Controllers/CodeVerificationController.cs
+++ WebApi/Controllers/CodeVerificationController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Dtos;
+﻿using Application.Common.Errors;
+using Application.Dtos;
 using Application.ServiceAbstractions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,16 +7,18 @@ namespace WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CodeVerificationController(ICodeVerificationService verificationService) : ControllerBase
+    public class CodeVerificationController(ICodeVerificationService verificationService) : BaseApiController
     {
         // validate code endpoint
         [HttpPost("validate-code")]
+        [ProducesResponseType(typeof(BaseToReturnDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<BaseToReturnDto>> ValidateCode([FromForm] VerifyCodeDto verifyCodeDto)
         {
             var result = await verificationService.ValidateCode(verifyCodeDto);
             return result.Map<ActionResult<BaseToReturnDto>>(
                 onSuccess: res => Ok(res),
-                onFailure: err => BadRequest(err)
+                onFailure: err => HandleFailure(err)
             );
         }
     }
