90a57009272cf695e40d37b7ab5ef5183fc67a1b
ayman <aymanehab2002@gmail.com>
2026-02-09 03:16:31 +0200

    refactor(application) fix to faluire method

--- Application/Extentions/ValidationExtentions.cs
+++ Application/Extentions/ValidationExtentions.cs
@@ -1,11 +1,12 @@
 ﻿using Application.Common.Errors;
 using Application.Shared;
+using FluentValidation.Results;
 
 namespace Application.Extentions
 {
     public static class ValidationExtentions
     {
-        public static Result<T> ToFailure<T>(this FluentValidation.Results.ValidationResult validationResult)
+        public static Result<T> ToFailure<T>(this ValidationResult validationResult)
         {
             var errors = validationResult.Errors
                 .Select(e => new Error(e.PropertyName, e.ErrorMessage))
