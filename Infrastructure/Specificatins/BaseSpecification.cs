550a12dd416b719822f4a53b841c13e45103d4a6
ayman <aymanehab2002@gmail.com>
2026-02-14 01:15:03 +0200

    feat(Infrastructure) : Adding base-specification-class

---
+++ Infrastructure/Specificatins/BaseSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace Infrastructure.Specificatins
+{
+    public abstract class BaseSpecification<TEntity>
+    {
+        protected BaseSpecification(Expression<Func<TEntity, bool>> criteria)
+            => Criteria = criteria;
+        public Expression<Func<TEntity, bool>> Criteria { get; } = default!;
+        public List<Expression<Func<TEntity, object>>> IncludeStatements { get; } = new();
+        public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
+        public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+            => IncludeStatements.Add(includeExpression);
+        protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression)
+            => OrderBy = orderByExpression;
+        protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescExpression)
+            => OrderByDescending = orderByDescExpression;
+
+    }
+}
