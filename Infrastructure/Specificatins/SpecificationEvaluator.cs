676ce0749fc3ea8889121947ecdffb63cc0cdbd6
ayman <aymanehab2002@gmail.com>
2026-02-14 01:38:04 +0200

    feat(infrastructure) : add Specification Evaluator helper class

---
+++ Infrastructure/Specificatins/SpecificationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Specificatins
+{
+    public static class SpecificationEvaluator
+    {
+        public static IQueryable<TEntity> GetQuery<TEntity>(IQueryable<TEntity> inputQuery,
+            BaseSpecification<TEntity> specification) where TEntity : class
+        {
+            var query = inputQuery;
+            if (specification.Criteria != null)
+                query = query.Where(specification.Criteria);
+            if (specification.IncludeExpressions.Any())
+            {
+                query = specification.IncludeExpressions.Aggregate(query,
+                (current, includeExpression)
+                => current.Include(includeExpression));
+            }
+            if (specification.OrderBy != null)
+                query = query.OrderBy(specification.OrderBy);
+            if (specification.OrderByDescending != null)
+                query = query.OrderByDescending(specification.OrderByDescending);
+            return query;
+        }
+    }
+}
