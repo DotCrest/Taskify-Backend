7092ecb6b23114754bed8e6e5bf836e458ccd282
ayman <aymanehab2002@gmail.com>
2026-02-14 01:36:13 +0200

    refactor(infrastructure) : add wher tentity is class in base-specification-class and rename Inlude statements to IncludeExpressions

--- Infrastructure/Specificatins/BaseSpecification.cs
+++ Infrastructure/Specificatins/BaseSpecification.cs
@@ -3,15 +3,16 @@
 namespace Infrastructure.Specificatins
 {
     public abstract class BaseSpecification<TEntity>
+        where TEntity : class
     {
         protected BaseSpecification(Expression<Func<TEntity, bool>> criteria)
             => Criteria = criteria;
         public Expression<Func<TEntity, bool>> Criteria { get; } = default!;
-        public List<Expression<Func<TEntity, object>>> IncludeStatements { get; } = new();
+        public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = new();
         public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
         public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
         protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
-            => IncludeStatements.Add(includeExpression);
+            => IncludeExpressions.Add(includeExpression);
         protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression)
             => OrderBy = orderByExpression;
         protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescExpression)
