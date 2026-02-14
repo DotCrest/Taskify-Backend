eb36737d9b099471f45f9ce2eac55cabd80b9787
ayman <aymanehab2002@gmail.com>
2026-02-15 00:13:13 +0200

    feat(Infrastructure) : add work-space-member-specification

---
+++ Infrastructure/Specificatins/WorkSpaceMemberSpecifications/WorkSpaceMemberSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+
+namespace Infrastructure.Specificatins.WorkSpaceMemberSpecifications
+{
+    public class WorkSpaceMemberSpecification : BaseSpecification<WorkspaceMember>
+    {
+        public WorkSpaceMemberSpecification(int workspaceId, string userId)
+            : base(wm => wm.WorkspaceId == workspaceId && wm.UserId == userId)
+        {
+            AddInclude(mw => mw.User);
+        }
+        public WorkSpaceMemberSpecification(int workspaceId)
+            : base(wm => wm.WorkspaceId == workspaceId)
+        {
+            AddInclude(mw => mw.User);
+        }
+    }
+}
