98ce5c5a696d16ecb63ca806eb456aab87fca123
BassimEhab <bassimstudy@gmail.com>
2026-02-15 04:24:58 +0200

    feat(WebApi): Register IAccountService with scoped lifetime
    Added IAccountService and its AccountService implementation to the dependency injection container using AddScoped.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -36,6 +36,7 @@ builder.Services.AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<ICodeVerificationService, CodeVerificationService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<PasswordHasher<User>>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.Configure<EmailOptions>(builder.Configuration.GetSection("email-config"));
