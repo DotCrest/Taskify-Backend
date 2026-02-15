38bd7bc8f23f1b2ee42f5dc6acb0b8e764e539d5
Bassem Ehab <121682008+BassimEhab@users.noreply.github.com>
2026-02-15 19:17:59 +0200

    Feature/database (#27)
    * refactor(Infrastructure): Refactor enum storage to use int instead of string
    
    Removed EnumToStringConverter for Status and Priority in Invitation and Quest entities. Updated EF Core configurations and added a migration to change related columns from string to int, standardizing enum storage as integers in the database.
    
    * feat(WebApi): Enable string serialization for enums in JSON responses
    
    Added JsonStringEnumConverter to controllers' JSON options, ensuring enums are serialized as strings instead of numbers in API responses. Also imported System.Text.Json.Serialization in Program.cs.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -13,15 +13,16 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(jo =>
+                    jo.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.Configure<ApiBehaviorOptions>(options =>
