22f45d72f4d217e57fe35b01224a0c4f47587707
ayman <aymanehab2002@gmail.com>
2026-02-16 01:46:08 +0200

    feat(application) :
    Introduced `IWorkSpaceService` interface in `Application.ServiceAbstractions` with a method `IsWorkSpaceExsist(int workSpaceId)` to verify workspace existence.
    
    Implemented the interface in `WorkSpaceService` within `Application.Services`. The class uses dependency injection to access `IUnitOfWork<Workspace>` and its repository.

---
+++ Application/Services/WorkSpaceService.cs
@@ -0,0 +1,20 @@
+﻿using Application.ServiceAbstractions;
+using Domain.Contracts;
+using Domain.Models;
+
+namespace Application.Services
+{
+    public class WorkSpaceService(IUnitOfWork<Workspace> unitOfWork) : IWorkSpaceService
+    {
+        private readonly IGenericRepository<Workspace> repo = unitOfWork.Repo;
+        public async Task<bool> IsWorkSpaceExsist(int workSpaceId)
+        {
+
+            var workSpace = await repo.GetByIdAsync(workSpaceId);
+            if (workSpace == null)
+                return false;
+            return true;
+
+        }
+    }
+}
