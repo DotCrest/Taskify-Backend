2b9707e612f73bde609106c18cb6769b0e411d92
BassimEhab <bassimstudy@gmail.com>
2026-02-15 04:22:55 +0200

    feat(Application): Add account service abstraction and implementation
    Introduced IAccountService interface with methods to get users by email and ID. Implemented AccountService using UserManager<User> to provide these functionalities. Added necessary namespaces and using directives.

---
+++ Application/Services/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using Application.ServiceAbstractions;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Services;
+
+public class AccountService(UserManager<User> userManager) : IAccountService
+{
+    public async Task<User?> GetUserByEmailAsync(string email)
+    {
+        var user = await userManager.FindByEmailAsync(email);
+        return user;
+    }
+
+    public async Task<User?> GetUserByIdAsync(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        return user;
+    }
+}
