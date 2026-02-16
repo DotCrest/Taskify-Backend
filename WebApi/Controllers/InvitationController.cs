0654da1134c9fd38fa6a24417b86605e29b72c68
BassimEhab <bassimstudy@gmail.com>
2026-02-16 23:47:38 +0200

    feat(WebApi): Add InvitationController with SendInvitation endpoint.
    Introduced InvitationController to handle invitation API requests.
    Includes SendInvitation action for Admins, with validation and
    service integration. Returns appropriate status codes and errors.

---
+++ WebApi/Controllers/InvitationController.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Errors;
+using Application.Dtos;
+using Application.Dtos.InvitationDtos;
+using Application.ServiceAbstractions;
+using Domain.Constants;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class InvitationController(IInvitationService invitationService,
+                                  IValidator<SendInvitationDto> sendInvitationValidator) : BaseApiController
+{
+    [Authorize(Role.Admin)]
+    [HttpPost("send-invitation")]
+    [ProducesResponseType(typeof(BaseToReturnDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<BaseToReturnDto>> SendInvitation(SendInvitationDto sendInvitationDto)
+    {
+        // validation
+        var validation = await ExecuteWithValidation(sendInvitationValidator, sendInvitationDto);
+        if (!validation.IsSuccess)
+            return HandleFailure(validation.ErrorsList);
+        // business logic
+        var senderId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await invitationService.SendInvitationAsync(sendInvitationDto, senderId!);
+        return result.Map<ActionResult<BaseToReturnDto>>(
+            onSuccess: res => Ok(res),
+            onFailure: err => HandleFailure(err)
+        );
+    }
+}
