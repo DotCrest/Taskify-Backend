049eaacaf77a39ad9af24657b4b120f2b4b135b9
BassimEhab <bassimstudy@gmail.com>
2026-02-17 04:59:43 +0200

    feat(Appliation): Add WorkspaceErrors with NotFound error definition.

---
+++ Application/Shared/Errors/WorkspaceErrors.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.Errors;
+
+namespace Application.Shared.Errors;
+
+public static class WorkspaceErrors
+{
+    public static readonly Error NotFound =
+            new Error("Workspace.NotFound", "Workspace is not found!");
+}
