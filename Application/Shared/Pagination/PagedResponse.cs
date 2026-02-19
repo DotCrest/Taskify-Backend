93e0ab8bda814ba199e06feb5b9369b6cfc2678f
Bassem Ehab <121682008+BassimEhab@users.noreply.github.com>
2026-02-19 19:13:23 +0200

    Feature/pagination (#46)
    * feat(Application): Add pagination support with PagedResponse and QueryFilter
    
    Introduced a new namespace `Application.Shared.Pagination` and
    added two classes: `PagedResponse<T>` and `QueryFilter`.
    
    `PagedResponse<T>` is a generic class for paginated data,
    including properties for data, page details, and navigation
    flags (`HasPreviousPage` and `HasNextPage`).
    
    `QueryFilter` provides a simple model for pagination input
    with `PageNumber` and `pageSize` properties, defaulting to
    1 and 10, respectively.
    
    * refactor(Domain): Refactor sorting in ISpecification interface
    
    Replaced `OrderBy` and `OrderByDescending` properties with a
    new `SortExpressions` property in the `ISpecification<TEntity>`
    interface.
    
    * refactor(Application): Refactor sorting logic in BaseSpecification
    
    remove`OrderBy` and
    `OrderByDescending` properties and their associated methods.
    Introduced a `SortExpressions` property to store sorting
    criteria as strings. Added a new `ApplySorting<T>` method
    to handle dynamic sorting with validation using reflection.
    Removed redundant code and improved maintainability.
    
    * refactor(Infrastructure): Refactor sorting logic with dynamic LINQ support
    
    Added `System.Linq.Dynamic.Core` to enable dynamic query construction.
    Refactored sorting logic in `SpecificationEvaluator.cs` to use
    dynamic LINQ for handling multiple sort expressions. Removed
    previous `OrderBy` and `OrderByDescending` logic. Pagination
    logic remains unchanged. These changes enhance flexibility and
    maintainability for runtime-determined sorting scenarios.
    
    * feat(Application): Add `SortBy` property to QueryFilter class.

---
+++ Application/Shared/Pagination/PagedResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Shared.Pagination;
+
+internal class PagedResponse<T> where T : class
+{
+    public IReadOnlyList<T> Data { get; init; } = [];
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalPages { get; init; }
+    public int TotalRecords { get; init; }
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+}
--- Application/Specifications/BaseSpecification.cs
+++ Application/Specifications/BaseSpecification.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Contracts;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Application.Specifications
 {
@@ -10,22 +11,35 @@ namespace Application.Specifications
             => Criteria = criteria;
         public Expression<Func<TEntity, bool>> Criteria { get; } = default!;
         public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = new();
-        public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
-        public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
         public int Skip { get; private set; }
         public int Take { get; private set; }
         public bool IsPaginated { get; private set; }
+        public List<string> SortExpressions { get; private set; } = [];
+
         protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
             => IncludeExpressions.Add(includeExpression);
-        protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression)
-            => OrderBy = orderByExpression;
-        protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescExpression)
-            => OrderByDescending = orderByDescExpression;
         protected void ApplyPagination(int pageSize, int pageIndex)
         {
             IsPaginated = true;
             Take = pageSize;
             Skip = (pageIndex - 1) * pageSize;
+        }
+        protected void ApplySorting<T>(string sortBy)
+        {
+            // the `sortBy` parameter is expected to be like that => priority asc, createdAt desc
+            var allowedProperties = typeof(T)
+                                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                    .Select(p => p.Name)
+                                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+            foreach (var part in sortBy.Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            {
+                // the `part` will be like that => priority asc
+                var tokens = part.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length == 0 || !allowedProperties.Contains(tokens[0]))
+                    continue;
+                var direction = tokens.Length > 1 && tokens[1].Equals("desc", StringComparison.OrdinalIgnoreCase) ? "descending" : "ascending";
+                SortExpressions.Add($"{tokens[0]} {direction}");
+            }
         }
     }
 }
