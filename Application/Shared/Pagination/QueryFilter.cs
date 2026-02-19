b253440039115024b95c75b9e2e855af8f1ac364
BassimEhab <bassimstudy@gmail.com>
2026-02-19 19:10:59 +0200

    feat(Application): Add `SortBy` property to QueryFilter class.

--- Application/Shared/Pagination/QueryFilter.cs
+++ Application/Shared/Pagination/QueryFilter.cs
@@ -3,5 +3,6 @@
 public class QueryFilter
 {
     public int PageNumber { get; set; } = 1;
-    public int pageSize { get; set; } = 10;
+    public int PageSize { get; set; } = 10;
+    public string? SortBy { get; set; }
 }
