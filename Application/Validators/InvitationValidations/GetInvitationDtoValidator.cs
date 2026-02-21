c20790d557b9447f2aae094b0663f6c6b4abdd8e
BassimEhab <bassimstudy@gmail.com>
2026-02-21 17:25:33 +0200

    feat(Application): Add GetInvitationDtoValidator for input validation
    Introduce GetInvitationDtoValidator to enforce validation rules
    for the GetInvitationDto object. Added rules to ensure that
    WorkspaceId is not empty and Status is one of the allowed
    values ("pending," "accepted," "expired"). This improves data integrity and reduces potential errors.

---
+++ Application/Validators/InvitationValidations/GetInvitationDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Dtos.InvitationDtos;
+using FluentValidation;
+
+namespace Application.Validators.InvitationValidations;
+
+public class GetInvitationDtoValidator : AbstractValidator<GetInvitationDto>
+{
+    public GetInvitationDtoValidator()
+    {
+        RuleFor(x => x.WorkspaceId)
+            .NotEmpty().WithMessage("WorkspaceId is required.");
+        RuleFor(x => x.Status)
+            .NotEmpty().WithMessage("Status is required.")
+            .Must(status => new[] { "pending", "accepted", "expired" }.Contains(status.ToLower()))
+            .WithMessage("Status must be either 'pending', 'accepted', or 'expired'.");
+    }
+
+}
