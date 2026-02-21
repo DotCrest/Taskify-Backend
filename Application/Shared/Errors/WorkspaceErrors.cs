e0300cd7d87137e9baafba91239bc0e046eb08bf
BassimEhab <bassimstudy@gmail.com>
2026-02-21 16:57:04 +0200

    feat(Application): Add `AccessDenied` error to WorkspaceErrors.

--- Application/Shared/Errors/WorkspaceErrors.cs
+++ Application/Shared/Errors/WorkspaceErrors.cs
@@ -6,4 +6,6 @@ public static class WorkspaceErrors
 {
     public static readonly Error NotFound =
             new Error("Workspace.NotFound", "Workspace is not found!");
+    public static readonly Error AccessDenied =
+       new Error("Workspace.AccessDenied", "You don't have access to this workspace!");
 }
