e63d27c0ab3978d881dfa664e03d1ea7b7373265
Bassem Ehab <121682008+BassimEhab@users.noreply.github.com>
2026-02-22 03:06:38 +0200

    Feature/specification design pattern (#50)
    * feat(Domain, Infrastructure): Add BulkUpdateAsync method to GenericRepository.
    
    Enhanced the `IGenericRepository` interface and `GenericRepository` class:
    - Added `BulkUpdateAsync` method to support bulk updates.
    - Implemented `BulkUpdateAsync` using `ExecuteUpdateAsync` for efficient updates.
    
    These changes improve performance by enabling bulk updates directly at the query level.
    
    ---------
    
    Co-authored-by: ayman <aymanehab2002@gmail.com>

--- Infrastructure/Repository/GenericRepository.cs
+++ Infrastructure/Repository/GenericRepository.cs
@@ -2,6 +2,7 @@
 using Infrastructure.context;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
 namespace Infrastructure.Repository
@@ -49,6 +50,12 @@ namespace Infrastructure.Repository
         public async Task BulkDeleteAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
         {
             await _dbSet.Where(predicate).ExecuteDeleteAsync(cancellationToken);
+        }
+
+        public async Task BulkUpdateAsync(ISpecification<T> specification, Expression<Func<SetPropertyCalls<T>, SetPropertyCalls<T>>> updateAction, CancellationToken cancellationToken = default)
+        {
+            var query = SpecificationEvaluator.CreateQuery(context.Set<T>(), specification);
+            await query.ExecuteUpdateAsync(updateAction, cancellationToken);
         }
     }
 }
