468f060ef3c0b5dce811c9b9e9c1d8b2a3fdd43e
ayman <aymanehab2002@gmail.com>
2026-02-23 02:20:57 +0200

    feat(application) : add workspace-get-all-specification

---
+++ Application/Specifications/WorkspaceSpecifications/WorkspaceGetAllSpecification.cs
@@ -0,0 +1,25 @@
+﻿using Application.Shared.Pagination;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Specifications.WorkspaceSpecifications
+{
+    public class WorkspaceGetAllSpecification : BaseSpecification<Workspace>
+    {
+        public WorkspaceGetAllSpecification(QueryFilter queryFilter)
+            :base(w => true)
+        {
+            AddInclude(w => w.User);
+            AddInclude(w=>w.WorkspaceMembers);
+            ApplyPagination(queryFilter.PageSize, queryFilter.PageNumber);
+            if(!string.IsNullOrEmpty(queryFilter.SortBy))
+                ApplySorting<Workspace>(queryFilter.SortBy);
+
+
+        }
+    }
+}
