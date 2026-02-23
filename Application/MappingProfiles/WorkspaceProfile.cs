68ace005175c51c0ca5ad44265e3e11eb1422343
ayman <aymanehab2002@gmail.com>
2026-02-23 02:19:46 +0200

    feat(application) : add workspace mapping profile

---
+++ Application/MappingProfiles/WorkspaceProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos.WorkspaceDtos;
+using AutoMapper;
+using Domain.Models;
+
+namespace Application.MappingProfiles
+{
+    public class WorkspaceProfile : Profile
+    {
+        public WorkspaceProfile()
+        {
+            CreateMap<Workspace, WorkspaceSimpleDto>()
+                .ForMember(dest => dest.OwnerName, opt =>
+                opt.MapFrom(src =>
+                src.User.Name))
+                .ForMember(dest => dest.MembersCount, opt =>
+                opt.MapFrom(src => src.WorkspaceMembers.Count));
+
+
+        }
+    }
+}
