be320d0303d8dc4754eb44c00af0e0d4c291e005
ayman <aymanehab2002@gmail.com>
2026-02-23 02:24:24 +0200

    feat(application) : add GetAllWorkspacesAsync signature to IWorkSpaceService

--- Application/ServiceAbstractions/IWorkSpaceService.cs
+++ Application/ServiceAbstractions/IWorkSpaceService.cs
@@ -1,9 +1,13 @@
-﻿using Domain.Models;
+﻿using Application.Dtos.WorkspaceDtos;
+using Application.Shared;
+using Application.Shared.Pagination;
+using Domain.Models;
 
 namespace Application.ServiceAbstractions
 {
     public interface IWorkSpaceService
     {
         Task<Workspace?> GetWorkSpaceById(int workSpaceId);
+        Task<Result<PagedResponse<WorkspaceSimpleDto>>> GetAllWorkspacesAsync(QueryFilter queryFilter);
     }
 }
