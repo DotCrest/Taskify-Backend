55e9005864ea179d621a14886cbd0b830bc7c3e9
ayman <aymanehab2002@gmail.com>
2026-02-25 04:04:45 +0200

    refactor(application): removing user id from creteria

--- Application/Specifications/WorkspaceSpecifications/WorkspaceGetByIdSpecification.cs
+++ Application/Specifications/WorkspaceSpecifications/WorkspaceGetByIdSpecification.cs
@@ -4,8 +4,8 @@ namespace Application.Specifications.WorkspaceSpecifications
 {
     public class WorkspaceGetByIdSpecification : BaseSpecification<Workspace>
     {
-        public WorkspaceGetByIdSpecification(int workspaceId, string userId)
-            : base(w => w.Id == workspaceId && w.OwnerId == userId || w.WorkspaceMembers.Any(m => m.UserId == userId))
+        public WorkspaceGetByIdSpecification(int workspaceId)
+            : base(w => w.Id == workspaceId)
         {
             AddInclude(w => w.WorkspaceMembers);
             AddInclude(w => w.User);
