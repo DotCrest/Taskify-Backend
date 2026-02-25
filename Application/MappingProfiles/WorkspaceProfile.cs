9ae3cc2c3ead62cacdd07933ff4f78d20cad6ce8
ayman <aymanehab2002@gmail.com>
2026-02-25 04:07:15 +0200

    faet(application): add the mapping profile for workspace details dto

--- Application/MappingProfiles/WorkspaceProfile.cs
+++ Application/MappingProfiles/WorkspaceProfile.cs
@@ -15,7 +15,13 @@ namespace Application.MappingProfiles
                 .ForMember(dest => dest.MembersCount, opt =>
                 opt.MapFrom(src => src.WorkspaceMembers.Count));
 
-
+            CreateMap<Workspace, WorkspaceDetailsDto>()
+               .ForMember(dest => dest.OwnerName, opt => opt.MapFrom(src => src.User.UserName))
+            .ForMember(dest => dest.Spaces, opt => opt.MapFrom(src => src.Spaces))
+            .ForMember(dest => dest.Members, opt => opt.MapFrom(src => src.WorkspaceMembers))
+            .ForMember(dest => dest.TotalSpaces, opt => opt.MapFrom(src => src.Spaces.Count))
+            .ForMember(dest => dest.TotalMembers, opt => opt.MapFrom(src => src.WorkspaceMembers.Count))
+            .ForMember(dest => dest.TotalTags, opt => opt.MapFrom(src => src.Tags.Count));
         }
     }
 }
