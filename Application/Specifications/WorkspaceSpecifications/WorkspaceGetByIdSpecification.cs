a706a7a2e13f48a3838b9c5683c7a9ca7f278a96
ayman <aymanehab2002@gmail.com>
2026-02-25 03:52:50 +0200

    feat(application): add workspace get by id specififcation

---
+++ Application/Specifications/WorkspaceSpecifications/WorkspaceGetByIdSpecification.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+
+namespace Application.Specifications.WorkspaceSpecifications
+{
+    public class WorkspaceGetByIdSpecification : BaseSpecification<Workspace>
+    {
+        public WorkspaceGetByIdSpecification(int workspaceId, string userId)
+            : base(w => w.Id == workspaceId && w.OwnerId == userId || w.WorkspaceMembers.Any(m => m.UserId == userId))
+        {
+            AddInclude(w => w.WorkspaceMembers);
+            AddInclude(w => w.User);
+            AddInclude(w => w.Spaces);
+            AddInclude(w => w.Tags);
+
+        }
+    }
+}
