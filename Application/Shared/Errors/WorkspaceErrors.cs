994b35dd69d0d7b30868412e3b7573dadb45a922
ayman <aymanehab2002@gmail.com>
2026-02-26 16:22:08 +0200

    feat(application): add created failed error to workspace errors

--- Application/Shared/Errors/WorkspaceErrors.cs
+++ Application/Shared/Errors/WorkspaceErrors.cs
@@ -8,4 +8,6 @@ public static class WorkspaceErrors
             new Error("Workspace.NotFound", "Workspace is not found!");
     public static readonly Error AccessDenied =
        new Error("Workspace.AccessDenied", "You don't have access to this workspace!");
+    public static readonly Error CreatedFailed =
+        new Error("Workspace.CreatedFailed", "An error occurred while creating the workspace");
 }
