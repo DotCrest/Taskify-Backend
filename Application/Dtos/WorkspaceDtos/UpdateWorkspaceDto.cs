ef27d616e55bd501d313c57bc65578de8b05a322
ayman <aymanehab2002@gmail.com>
2026-02-27 02:47:52 +0200

    feat(application): add update workspace dto

---
+++ Application/Dtos/WorkspaceDtos/UpdateWorkspaceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Dtos.WorkspaceDtos
+{
+    public class UpdateWorkspaceDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Avatar { get; set; }
+    }
+}
