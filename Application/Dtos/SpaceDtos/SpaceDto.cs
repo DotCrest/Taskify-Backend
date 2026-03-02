16b01335fbfcded6cb5ea0b5f51517af8e88dacb
BassimEhab <bassimstudy@gmail.com>
2026-03-02 03:40:28 +0200

    feat(Application): Add CreateSpaceDto and SpaceDto classes for Spaces.
    Introduced two new DTO classes :
    - CreateSpaceDto for creating new Space entities, including workspace and icon details.
    - SpaceDto for representing Space entities with id, name, workspace, and icon properties.

---
+++ Application/Dtos/SpaceDtos/SpaceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Dtos.SpaceDtos;
+
+public class SpaceDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? IconColor { get; set; }
+    public string? IconType { get; set; }
+    public string? IconValue { get; set; }
+    public int WorkspaceId { get; set; }
+}
