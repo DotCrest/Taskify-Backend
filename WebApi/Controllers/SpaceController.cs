49751eabc3155aaea9f56adf317ee5c11ccd45f9
BassimEhab <bassimstudy@gmail.com>
2026-03-02 14:43:47 +0200

    feat(WebApi): Add paginated get-all endpoint to SpaceController.

--- WebApi/Controllers/SpaceController.cs
+++ WebApi/Controllers/SpaceController.cs
@@ -1,10 +1,12 @@
 ﻿using Application.Common.Errors;
 using Application.Dtos.SpaceDtos;
 using Application.ServiceAbstractions;
+using Application.Shared.Pagination;
 using Domain.Constants;
 using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace WebApi.Controllers;
 [ApiController]
@@ -27,6 +29,19 @@ public class SpaceController(ISpaceService spaceService,
         if (!result.IsSuccess)
             return HandleFailure(result.ErrorsList);
         return result.Map<ActionResult<SpaceDto>>(
+            onSuccess: res => Ok(res),
+            onFailure: err => HandleFailure(err));
+    }
+    [HttpGet("get-all")]
+    [Authorize]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(PagedResponse<SpaceDto>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PagedResponse<SpaceDto>>> GetAllSpaces([FromQuery] int workspaceId, [FromQuery] QueryFilter queryFilter)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await spaceService.GetSpacesByWorkspaceIdAsync(workspaceId, userId!, queryFilter);
+        return result.Map<ActionResult<PagedResponse<SpaceDto>>>(
             onSuccess: res => Ok(res),
             onFailure: err => HandleFailure(err));
     }
