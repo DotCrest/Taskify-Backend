1d8fba15320c60fb6d6e14d2b8d1addedcc1de5a
BassimEhab <bassimstudy@gmail.com>
2026-03-02 14:35:10 +0200

    feat(Application): Add AccessDenied error to SpaceErrors class.

--- Application/Shared/Errors/SpaceErrors.cs
+++ Application/Shared/Errors/SpaceErrors.cs
@@ -9,4 +9,6 @@ public static class SpaceErrors
         new Error("Space.NotFound", "The specified space was not found.");
     public static Error AlreadyExists =
         new Error("Space.AlreadyExists", "Space with the same name already exists.");
+    public static Error AccessDenied =
+        new Error("Space.AccessDenied", "You do not have permission to access these spaces.");
 }
