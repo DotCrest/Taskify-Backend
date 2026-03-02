c7c66c2d674c6b41a64353a355703ebb531d00e6
BassimEhab <bassimstudy@gmail.com>
2026-03-02 03:45:59 +0200

    feat(Application): Add ISpaceService and SpaceService for space creation.
    Introduce ISpaceService interface and its implementation SpaceService to handle creation of Space entities. Includes validation for workspace existence and duplicate space names, sets default icon values, and returns results using DTOs and error handling.

---
+++ Application/ServiceAbstractions/ISpaceService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Dtos.SpaceDtos;
+using Application.Shared;
+
+namespace Application.ServiceAbstractions;
+
+public interface ISpaceService
+{
+    public Task<Result<SpaceDto>> CreateSpaceAsync(CreateSpaceDto createSpaceDto);
+}
---
+++ Application/Services/SpaceService.cs
@@ -0,0 +1,39 @@
+﻿using Application.Dtos.SpaceDtos;
+using Application.ServiceAbstractions;
+using Application.Shared;
+using Application.Shared.Errors;
+using AutoMapper;
+using Domain.Contracts;
+using Domain.Models;
+
+namespace Application.Services;
+
+public class SpaceService(IUnitOfWork unitOfWork,
+                          IMapper mapper) : ISpaceService
+{
+    private readonly IGenericRepository<Space> spaceRepo = unitOfWork.Repository<Space>();
+    private readonly IGenericRepository<Workspace> workspaceRepo = unitOfWork.Repository<Workspace>();
+    public async Task<Result<SpaceDto>> CreateSpaceAsync(CreateSpaceDto createSpaceDto)
+    {
+        var workspace = await workspaceRepo.GetByIdAsync(createSpaceDto.WorkspaceId);
+        if (workspace is null)
+            return Result<SpaceDto>.Failure(WorkspaceErrors.NotFound);
+        var existedSpace = await spaceRepo.Find(s => s.Name == createSpaceDto.Name && s.WorkspaceId == createSpaceDto.WorkspaceId);
+        if (existedSpace is not null)
+            return Result<SpaceDto>.Failure(SpaceErrors.AlreadyExists);
+        var space = new Space
+        {
+            Name = createSpaceDto.Name,
+            // set default values
+            IconColor = string.IsNullOrEmpty(createSpaceDto.IconColor) ? "#808080" : createSpaceDto.IconColor,
+            IconType = string.IsNullOrEmpty(createSpaceDto.IconType) ? "material-symbols-outlined" : createSpaceDto.IconType,
+            IconValue = string.IsNullOrEmpty(createSpaceDto.IconValue) ? "label" : createSpaceDto.IconValue,
+            WorkspaceId = createSpaceDto.WorkspaceId,
+            CreatedAt = DateTime.UtcNow,
+        };
+        await spaceRepo.AddAsync(space);
+        await unitOfWork.SaveAsync();
+        var spaceToReturn = mapper.Map<SpaceDto>(space);
+        return Result<SpaceDto>.Success(spaceToReturn);
+    }
+}
