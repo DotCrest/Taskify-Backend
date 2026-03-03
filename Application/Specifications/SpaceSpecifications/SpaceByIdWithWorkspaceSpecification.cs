3ed9f308a5fa556c19660d6f6e6e93ee2d98c344
BassimEhab <bassimstudy@gmail.com>
2026-03-03 03:57:09 +0200

    feat(Application): Add SpaceByIdWithWorkspaceSpecification class.

---
+++ Application/Specifications/SpaceSpecifications/SpaceByIdWithWorkspaceSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Application.Specifications.SpaceSpecifications
+{
+    public class SpaceByIdWithWorkspaceSpecification : BaseSpecification<Space>
+    {
+        public SpaceByIdWithWorkspaceSpecification(int spaceId) : base(x => x.Id == spaceId)
+        {
+            AddInclude(x => x.Workspace);
+        }
+    }
+}
