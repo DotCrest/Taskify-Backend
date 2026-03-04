a051829a91893ce0130c4fb330484a1d323a86a0
BassimEhab <bassimstudy@gmail.com>
2026-03-04 05:00:02 +0200

    feat(Application): Add NotFound error to TagErrors.

--- Application/Shared/Errors/TagErrors.cs
+++ Application/Shared/Errors/TagErrors.cs
@@ -8,4 +8,6 @@ public class TagErrors
         new Error("Tag.AccessDenied", "You do not have permission to create or access tags.");
     public static Error AlreadyExisted =
         new Error("Tag.AlreadyExisted", "A tag with the same name already exists in this workspace.");
+    public static Error NotFound =
+        new Error("Tag.NotFound", "The specified tag was not found.");
 }
