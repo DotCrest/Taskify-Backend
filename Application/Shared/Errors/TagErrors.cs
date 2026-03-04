10d263350b38c993f77953066fca7e586cc347ca
BassimEhab <bassimstudy@gmail.com>
2026-03-04 03:25:59 +0200

    feat(Application): Add TagErrors class.

---
+++ Application/Shared/Errors/TagErrors.cs
@@ -0,0 +1,9 @@
+﻿using Application.Common.Errors;
+
+namespace Application.Shared.Errors;
+
+public class TagErrors
+{
+    public static Error AccessDenied =
+        new Error("Tag.AccessDenied", "You do not have permission to create or access tags.");
+}
