3b525bb91bcd56fcdc705706d932ce027048e8f8
Bassem Ehab <121682008+BassimEhab@users.noreply.github.com>
2026-03-04 06:31:19 +0200

    Feature/quest (#65)
    ----------------------------------------------- <MAINLY ABOUT TAG SERVICE> ----------------------------------------------
    * feat(Application): Add QuestsByCategory specification.
    
    Added specification to filter quests by category IDs.
    Removed QuestsByWorkspace specification.
    
    * feat(Application): Add CategoryByWorkspaceSpecification for workspace filtering.
    
    * refactor(Application): Replace quest detachment with bulk category update
    
    Refactored IQuestService and QuestService to remove DetachQuestFromWorkspace and introduce BulkUpdateQuestCategoryAsync. The new method updates the category for multiple quests in a workspace using category and quest specifications.
    
    * refactor(Application): Rename quest detach with BulkUpdateQuestCategoryAsync.
    
    * feat(Application): Add TagDto and TagToReturnDto for tag data transfer.
    
    * feat(Application): Add TagErrors class.
    
    * feat(Application): Add AutoMapper profile for Tag and Tag DTO mapping.
    
    * feat(Application): Add CreateTagAsync to ITagService and TagService.
    
    Implement tag creation with workspace/user validation, error handling, and AutoMapper.
    Refactor repository naming.
    
    * feat(Application): Add TagDtoValidator for hex color and workspace ID validation.
    
    * feat(WebApi): Add TagController with tag creation endpoint.
    
    Implemented TagController as an API endpoint for tag operations. Added CreateTagAsync POST method with validation, user ID extraction, and error handling for various response scenarios. Requires authorization.
    
    * feat(Application): Add AlreadyExisted error to TagErrors class.
    
    * feat(Application): Add TagByNameAndWorkspaceSpecification class.
    
    * feat(Application): Refactor TagService validation and duplicate checks.
    
    Refactored CreateTagAsync to use helper methods for workspace existence, user access, and duplicate tag checks. Improved code modularity and error handling by introducing CheckWorkspaceExistenceAndUserAccessAsync and CheckIfTagAlreadyExists private methods. Prevents creation of duplicate tags within a workspace.
    
    * feat(Application): Add Tag specifications for workspace filtering and queries.
    
    - Added TagByWorkspaceCountSpecification for counting tags in a workspace.
    - Introduced TagByWorkspaceSpecification to support workspace-based tag queries with pagination and sorting.
    
    * feat(Application): Add paginated tag retrieval to ITagService and TagService.
    
    * feat(WebApi): Add paginated GET endpoint for tags in TagController.
    
    * feat(Application): Add NotFound error to TagErrors.
    
    * feat(Application): Add GetTagByIdAsync to ITagService and TagService
    
    Introduce GetTagByIdAsync method to retrieve a tag by its ID for a specific user. The method checks for tag existence and validates user access to the associated workspace before returning the tag or appropriate errors.
    
    * feat(WebApi): Add GET endpoint to fetch tag by ID in TagController.
    
    * feat(Application): Add DeleteTagByIdAsync to ITagService and TagService.
    
    Introduce DeleteTagByIdAsync method to enable tag deletion by ID with user access validation.
    
    * feat(WebApi): Add DELETE endpoint to TagController for tag removal.
    
    ---------
    
    Co-authored-by: ayman <aymanehab2002@gmail.com>

---
+++ Application/Specifications/QuestSpecifications/QuestsByCategorySpecification.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Application.Specifications.QuestSpecifications
+{
+    public class QuestsByCategorySpecification : BaseSpecification<Quest>
+    {
+        public QuestsByCategorySpecification(List<int> categoryIds) : base(q => q.CategoryId != (int?)null && categoryIds.Contains((int)q.CategoryId))
+        {
+
+        }
+    }
+}
