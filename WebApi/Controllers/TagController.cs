eaae7933b64318139d4c634aabaf6c12fd6c2829
BassimEhab <bassimstudy@gmail.com>
2026-03-04 05:02:44 +0200

    feat(WebApi): Add GET endpoint to fetch tag by ID in TagController.

--- WebApi/Controllers/TagController.cs
+++ WebApi/Controllers/TagController.cs
@@ -47,5 +47,18 @@ public class TagController(ITagService tagService,
             onSuccess: tags => Ok(tags),
             onFailure: err => HandleFailure(err));
     }
-
+    [HttpGet("{tagId}")]
+    [Authorize]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<Error>), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(TagToReturnDto), StatusCodes.Status200OK)]
+    public async Task<ActionResult<TagToReturnDto>> GetTagByIdAsync(int tagId)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await tagService.GetTagByIdAsync(tagId, userId!);
+        return result.Map<ActionResult<TagToReturnDto>>(
+            onSuccess: tag => Ok(tag),
+            onFailure: err => HandleFailure(err));
+    }
 }
