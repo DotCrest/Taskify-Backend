b6fec570c600f9be80d251e3ef250803e4101788
BassimEhab <bassimstudy@gmail.com>
2026-03-04 03:28:55 +0200

    feat(Application): Add CreateTagAsync to ITagService and TagService.
    Implement tag creation with workspace/user validation, error handling, and AutoMapper.
    Refactor repository naming.

--- Application/ServiceAbstractions/ITagService.cs
+++ Application/ServiceAbstractions/ITagService.cs
@@ -1,7 +1,11 @@
-﻿namespace Application.ServiceAbstractions
+﻿using Application.Dtos.TagDtos;
+using Application.Shared;
+
+namespace Application.ServiceAbstractions
 {
     public interface ITagService
     {
         Task DeleteAllTagsRelatedToWorkspace(int workSpaceId, CancellationToken cancellationToken = default);
+        Task<Result<TagToReturnDto>> CreateTagAsync(TagDto tagDto, string userId);
     }
 }
--- Application/Services/TagService.cs
+++ Application/Services/TagService.cs
@@ -1,15 +1,38 @@
-﻿using Application.ServiceAbstractions;
+﻿using Application.Dtos.TagDtos;
+using Application.ServiceAbstractions;
+using Application.Shared;
+using Application.Shared.Errors;
+using Application.Specifications.WorkspaceSpecifications;
+using AutoMapper;
 using Domain.Contracts;
 using Domain.Models;
 
 namespace Application.Services
 {
-    public class TagService(IUnitOfWork unitOfWork) : ITagService
+    public class TagService(IUnitOfWork unitOfWork,
+                            IMapper mapper) : ITagService
     {
-        private readonly IGenericRepository<Tag> repository = unitOfWork.Repository<Tag>();
+        private readonly IGenericRepository<Tag> tagRepo = unitOfWork.Repository<Tag>();
+        private readonly IGenericRepository<Workspace> workspaceRepo = unitOfWork.Repository<Workspace>();
+        public async Task<Result<TagToReturnDto>> CreateTagAsync(TagDto tagDto, string userId)
+        {
+            var workspaceSpecification = new WorkspaceGetByIdSpecification(tagDto.WorkspaceId);
+            var workspace = await workspaceRepo.Find(workspaceSpecification);
+            if (workspace == null)
+                return Result<TagToReturnDto>.Failure(WorkspaceErrors.NotFound);
+            var isMember = workspace.WorkspaceMembers.Any(x => x.UserId == userId);
+            if (!isMember)
+                return Result<TagToReturnDto>.Failure(TagErrors.AccessDenied);
+            var tag = mapper.Map<Tag>(tagDto);
+            await tagRepo.AddAsync(tag);
+            await unitOfWork.SaveAsync();
+            var tagToReturnDto = mapper.Map<TagToReturnDto>(tag);
+            return Result<TagToReturnDto>.Success(tagToReturnDto);
+        }
+
         public async Task DeleteAllTagsRelatedToWorkspace(int workSpaceId, CancellationToken cancellationToken = default)
         {
-            await repository.BulkDeleteAsync(t => t.WorkspaceId == workSpaceId, cancellationToken);
+            await tagRepo.BulkDeleteAsync(t => t.WorkspaceId == workSpaceId, cancellationToken);
         }
     }
 }
