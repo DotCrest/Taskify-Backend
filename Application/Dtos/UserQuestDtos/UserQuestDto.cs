00c3c56c2cea992b0cdf7eb515234c1f06e940ef
ayman <aymanehab2002@gmail.com>
2026-03-09 01:41:51 +0200

    feat(application) : create userQuest Dto

---
+++ Application/Dtos/UserQuestDtos/UserQuestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Dtos.UserQuestDtos
+{
+    public class UserQuestDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string? Avatag { get; set; }
+    }
+}
