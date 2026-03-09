38a3ad3fa8eb463b3751475f575c47daadae5df8
ayman <aymanehab2002@gmail.com>
2026-03-10 01:42:55 +0200

    feat(application) : add Get all quest specification

---
+++ Application/Specifications/QuestSpecifications/GetAllQuestSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Application.Shared.Pagination;
+using Domain.Models;
+
+namespace Application.Specifications.QuestSpecifications
+{
+    public class GetAllQuestSpecification : BaseSpecification<Quest>
+    {
+        public GetAllQuestSpecification(QueryFilter queryFilter, int spaceId)
+            : base(q => q.SpaceId == spaceId)
+        {
+            AddInclude(q => q.Category);
+
+            AddInclude(q => q.Tags);
+            AddInclude(q => q.Author);
+            ApplyPagination(queryFilter.PageSize, queryFilter.PageNumber);
+            if (!string.IsNullOrEmpty(queryFilter.SortBy))
+                ApplySorting<Quest>(queryFilter.SortBy);
+        }
+    }
+}
