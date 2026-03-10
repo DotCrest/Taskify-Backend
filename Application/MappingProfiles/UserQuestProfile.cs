c67d274853081777b0cf975fced2dc36e78977d7
ayman <aymanehab2002@gmail.com>
2026-03-10 02:41:30 +0200

    feat(application) : add mapping profiles fro quest and questuser

---
+++ Application/MappingProfiles/UserQuestProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos.UserQuestDtos;
+using AutoMapper;
+using Domain.Models;
+
+namespace Application.MappingProfiles
+{
+    public class UserQuestProfile : Profile
+    {
+        public UserQuestProfile()
+        {
+            CreateMap<UserQuest, UserQuestDto>()
+                .ForMember(dest => dest.UserName,
+                opt => opt.MapFrom(src => src.User.UserName))
+                .ForMember(dest => dest.Avatar,
+                opt => opt.MapFrom(src => src.User!.PhotoUrl))
+                .ForMember(dest => dest.UserId,
+                opt => opt.MapFrom(src => src.User.Id));
+
+        }
+    }
+}
