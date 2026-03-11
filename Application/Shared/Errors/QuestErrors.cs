9bd0bd75a422b5ed7380b0c555e804d14e8e4392
ayman <aymanehab2002@gmail.com>
2026-03-11 02:44:31 +0200

    feat(application) : add Quest Error class

---
+++ Application/Shared/Errors/QuestErrors.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Errors;
+
+namespace Application.Shared.Errors
+{
+    public static class QuestErrors
+    {
+        private static readonly Error NotFound
+            = new("Quest.NotFound", "The specified quest was not found.");
+    }
+}
