941e576cc611c16b01f7620231b7f8471d6514d5
ayman <aymanehab2002@gmail.com>
2026-03-11 02:47:37 +0200

    feta(application) : add get quest by id specifications

---
+++ Application/Specifications/QuestSpecifications/GetQuestByIdSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+
+namespace Application.Specifications.QuestSpecifications
+{
+    public class GetQuestByIdSpecification : BaseSpecification<Quest>
+    {
+        public GetQuestByIdSpecification(int spaceId)
+            : base(q => q.SpaceId == spaceId)
+        {
+            AddInclude(q => q.Category);
+            AddInclude(q => q.Assignees);
+            AddInclude(q => q.Tags);
+            AddInclude(q => q.Author);
+        }
+    }
+}
