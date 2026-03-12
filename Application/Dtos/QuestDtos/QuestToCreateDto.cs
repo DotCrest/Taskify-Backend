01bfb2944751a78b0aba5444a5d095a4770ee37e
ayman <aymanehab2002@gmail.com>
2026-03-13 01:09:57 +0200

    feat(application) : add QuestToCreateDto

---
+++ Application/Dtos/QuestDtos/QuestToCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.QuestDtos
+{
+    public class QuestToCreateDto
+    {
+        public string Title { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? CategoryId { get; set; }
+        public int Status { get; set; }
+        public int Priority { get; set; }
+    }
+}
