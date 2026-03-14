b796e9450a646e997040ac493fe2dd0747783cd7
ayman <aymanehab2002@gmail.com>
2026-03-14 03:28:37 +0200

    feat(application) : add Update quest validator

--- Application/ServiceAbstractions/IQuestService.cs
+++ Application/ServiceAbstractions/IQuestService.cs
@@ -10,5 +10,6 @@ public interface IQuestService
     Task<Result<PagedResponse<QuestToReturnDto>>> GetAllQuests(string userId, int spaceId, QueryFilter queryFilter);
     Task<Result<QuestToReturnDto>> GetQuestByIdAsync(string userId, int questId, int spaceId);
     Task<Result<QuestToReturnDto>> CreateQuestAsync(string userId, QuestToCreateDto createQuestDto, int spaceId);
-    Task<Result<bool>> UpdateQuestAsync(string userId, QuestToUpdateDto updateQuestDto, int spaceId);
+    Task<Result<bool>> UpdateQuestAsync(string userId, int questId, int spaceId, QuestToUpdateDto updateQuestDto);
+
 }
