d3ea3332f4edb00ecc44df6e15f231982b16ab7a
BassimEhab <bassimstudy@gmail.com>
2026-04-02 22:16:15 +0200

    feat(): Make error properties readonly and add new workspace error
    Updated `SpaceErrors` and `UserQuestErrors` to use `readonly static` for error properties, ensuring immutability and thread safety.
    
    Added a new error, `UserNotInWorkspace`, to `WorkspaceErrors` to handle cases where a user is not a member of the workspace. This aligns with the existing error definition pattern and improves functionality.

--- Application/Shared/Errors/SpaceErrors.cs
+++ Application/Shared/Errors/SpaceErrors.cs
@@ -5,10 +5,10 @@ namespace Application.Shared.Errors;
 
 public static class SpaceErrors
 {
-    public static Error NotFound =
+    public readonly static Error NotFound =
         new Error("Space.NotFound", "The specified space was not found.");
-    public static Error AlreadyExists =
+    public readonly static Error AlreadyExists =
         new Error("Space.AlreadyExists", "Space with the same name already exists.");
-    public static Error AccessDenied =
+    public readonly static Error AccessDenied =
         new Error("Space.AccessDenied", "You do not have permission to access these spaces.");
 }
--- Application/Shared/Errors/UserQuestErrors.cs
+++ Application/Shared/Errors/UserQuestErrors.cs
@@ -4,6 +4,8 @@ namespace Application.Shared.Errors;
 
 internal class UserQuestErrors
 {
-    public static Error UserAlreadyAssigned => new Error("UserQuest.UserAlreadyAssigned", "User is already assigned to this quest.");
-    public static Error UserNotAssignedToQuest => new Error("UserQuest.UserNotAssignedToQuest", "User is not assigned to this quest.");
+    public readonly static Error UserAlreadyAssigned
+        = new Error("UserQuest.UserAlreadyAssigned", "User is already assigned to this quest.");
+    public readonly static Error UserNotAssignedToQuest
+        = new Error("UserQuest.UserNotAssignedToQuest", "User is not assigned to this quest.");
 }
