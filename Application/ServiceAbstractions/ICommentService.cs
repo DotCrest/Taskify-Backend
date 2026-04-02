9dbc177ca28273891c30a97ed83e4b66ff5ab9fc
BassimEhab <bassimstudy@gmail.com>
2026-04-02 20:51:55 +0200

    feat(Application): Add AddCommentAsync method.
    Implemented AddCommentAsync in CommentService to handle adding comments. with todo validations.

--- Application/ServiceAbstractions/ICommentService.cs
+++ Application/ServiceAbstractions/ICommentService.cs
@@ -1,5 +1,10 @@
-﻿namespace Application.ServiceAbstractions;
+﻿using Application.Dtos.CommentDto;
+using Application.Shared;
+
+namespace Application.ServiceAbstractions;
 
 public interface ICommentService
 {
+    Task<Result<CommentDto>> AddCommentAsync(AddCommentDto addCommentDto);
+
 }
--- Application/Services/CommentService.cs
+++ Application/Services/CommentService.cs
@@ -1,7 +1,24 @@
-﻿using Application.ServiceAbstractions;
+﻿using Application.Dtos.CommentDto;
+using Application.ServiceAbstractions;
+using Application.Shared;
+using AutoMapper;
+using Domain.Contracts;
+using Domain.Models;
 
 namespace Application.Services;
 
-public class CommentService : ICommentService
+public class CommentService(IUnitOfWork unitOfWork,
+                            IMapper mapper) : ICommentService
 {
+    private readonly IGenericRepository<Comment> _commentRepository = unitOfWork.Repository<Comment>();
+    public async Task<Result<CommentDto>> AddCommentAsync(AddCommentDto addCommentDto)
+    {
+        // TODO: validate quest existince 
+        // TODO: check if user is assigned to this quest
+        var comment = mapper.Map<Comment>(addCommentDto);
+        await _commentRepository.AddAsync(comment);
+        await unitOfWork.SaveAsync();
+        var commentDto = mapper.Map<CommentDto>(comment);
+        return Result<CommentDto>.Success(commentDto);
+    }
 }
