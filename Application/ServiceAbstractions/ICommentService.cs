610becf055dbd962b911bb035da75f843875b288
Bassem Ehab <121682008+BassimEhab@users.noreply.github.com>
2026-04-04 18:00:59 +0200

    Feature/comment (#72)
    * feat(Application): Add new errors to CommentErrors.
    
    Enhanced the `CommentErrors` class by adding two new error
    definitions: `NotFound` for handling cases where a comment
    is not found, and `EditTimeout` for scenarios where the
    edit time window has expired.
    
    * feat(Application)Add method to fetch comments by questId.
    
    * feat(): Add support for updating comments
    
    Introduced `UpdateCommentDto` to handle comment updates.
    Updated `CommentProfile` to map `UpdateCommentDto` to `Comment`, with conditional mapping for `UserComment` and automatic updates to `UpdatedAt`.
    
    Modified `ICommentService` to include `UpdateCommentAsync` method. Implemented the method in `CommentService` with validations for comment existence, user access, and edit timeout. Updated the repository and saved changes to the database.
    
    * feat(Application): Add DeleteCommentAsync method to ICommentService.
    
    Implemented the `DeleteCommentAsync` method in the `ICommentService` interface and its corresponding logic in the `CommentService` class.
    
    The method allows users to delete comments by their ID, with validation to ensure:
    - The comment exists.
    - The user requesting the deletion is the owner of the comment.

--- Application/ServiceAbstractions/ICommentService.cs
+++ Application/ServiceAbstractions/ICommentService.cs
@@ -6,5 +6,7 @@ namespace Application.ServiceAbstractions;
 public interface ICommentService
 {
     Task<Result<CommentDto>> AddCommentAsync(AddCommentDto addCommentDto);
-
+    Task<Result<IEnumerable<CommentDto>>> GetCommentsByPlanIdAsync(int questId);
+    Task<Result<CommentDto>> UpdateCommentAsync(UpdateCommentDto updateCommentDto);
+    Task<Result<bool>> DeleteCommentAsync(int commentId, string userId);
 }
