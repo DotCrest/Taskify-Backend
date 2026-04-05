2164ceb9172f031692bce474a56ff2d1e81f7563
BassimEhab <bassimstudy@gmail.com>
2026-04-05 15:11:00 +0200

    refactor(Application): Refactor quest filtering to support advanced queries.
    Updated `IQuestService` and `QuestService` to use the new `QuestCustomQueryFilter` for enhanced filtering capabilities. Introduced `QuestCustomQueryFilter` class with additional properties (`CategoryId`, `TagId`, `CreatedAt`, `UpdatedAt`, `DueDate`) to enable more granular filtering. Updated `GetAllQuestSpecification` to handle the new filter properties and improve query precision. These changes enhance the flexibility and functionality of the quest retrieval process.

--- Application/ServiceAbstractions/IQuestService.cs
+++ Application/ServiceAbstractions/IQuestService.cs
@@ -7,7 +7,7 @@ namespace Application.ServiceAbstractions;
 public interface IQuestService
 {
     Task BulkUpdateQuestCategoryAsync(int workspaceId, int? categoryId = (int?)null);
-    Task<Result<PagedResponse<QuestToReturnDto>>> GetAllQuests(string userId, int spaceId, QueryFilter queryFilter);
+    Task<Result<PagedResponse<QuestToReturnDto>>> GetAllQuests(string userId, int spaceId, QuestCustomQueryFilter queryFilter);
     Task<Result<QuestToReturnDto>> GetQuestByIdAsync(string userId, int questId, int spaceId);
     Task<Result<QuestToReturnDto>> CreateQuestAsync(string userId, QuestToCreateDto createQuestDto, int spaceId);
     Task<Result<bool>> UpdateQuestAsync(string userId, int questId, int spaceId, QuestToUpdateDto updateQuestDto);
