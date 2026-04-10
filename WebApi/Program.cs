99a59efeaaec95ca1886ab4b7f5605d3909d7424
BassimEhab <bassimstudy@gmail.com>
2026-04-10 08:25:49 +0200

    feat(WebApi): Enable CORS and static file serving in the application.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -31,6 +31,18 @@ builder.Services.AddControllers()
     .AddJsonOptions(jo =>
                     jo.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
     );
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OpenCors", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.Configure<ApiBehaviorOptions>(options =>
@@ -165,6 +177,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("OpenCors");
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
