1faaf19a7d07c4d2d60a614d3a5f1e111479d149
BassimEhab <bassimstudy@gmail.com>
2026-04-10 18:17:53 +0200

    feat(WebApi): Add CacheAttribute for caching HTTP responses.
    Introduce CacheAttribute as a custom action filter to enable caching in ASP.NET Core applications. The attribute uses `ICacheService` to cache HTTP responses based on request paths and query parameters.
    
    - Added `TimeToLiveInSec` parameter to specify cache duration (default: 90 seconds).
    - Implemented `OnActionExecutionAsync` to handle caching logic.
    - Created `CreateKey` method to generate unique cache keys.

---
+++ WebApi/ActionFilters/CacheAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.ActionFilters;
+
+public class CacheAttribute(int TimeToLiveInSec = 90) : ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var cacheService = context.HttpContext.RequestServices.GetRequiredService<ICacheService>();
+
+        var key = CreateKey(context.HttpContext.Request);
+
+        var cachedValue = cacheService.GetData(key);
+
+        if (!string.IsNullOrEmpty(cachedValue))
+        {
+            context.Result = new ContentResult
+            {
+                Content = cachedValue,
+                ContentType = "application/json",
+                StatusCode = StatusCodes.Status200OK
+            };
+            return;
+        }
+        var executedContext = await next.Invoke();
+        if (executedContext.Result is OkObjectResult result)
+        {
+            cacheService.SetData(key, result.Value, TimeToLiveInSec);
+        }
+    }
+
+    private string CreateKey(HttpRequest request)
+    {
+        var path = request.Path.ToString();
+        var queryStrings = request.Query.OrderBy(x => x.Key);
+        var key = $"{path}?" + string.Join("&", queryStrings.Select(x => $"{x.Key}={x.Value}"));
+        return key;
+    }
+}
+
