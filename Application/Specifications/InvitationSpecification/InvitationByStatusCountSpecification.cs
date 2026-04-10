e597447463dee59114a7e78f5137352c7a3488c3
BassimEhab <bassimstudy@gmail.com>
2026-04-10 21:08:14 +0200

    remove(Application): Remove Invitation by status specifications.
    Removed `InvitationByStatusCountSpecification` and
    `InvitationByStatusSpecification` classes.

--- Application/Specifications/InvitationSpecification/InvitationByStatusCountSpecification.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domain.Models;
-
-namespace Application.Specifications.InvitationSpecification;
-
-public class InvitationByStatusCountSpecification : BaseSpecification<Invitation>
-{
-    public InvitationByStatusCountSpecification(InvitationStatusEnum status) : base(invitation => invitation.Status == status) { }
-}
