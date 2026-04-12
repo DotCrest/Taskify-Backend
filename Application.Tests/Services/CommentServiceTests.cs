f6e11bf43f29b146e5bbb0edae6ca5851651da89
BassimEhab <bassimstudy@gmail.com>
2026-04-12 10:37:32 +0200

    test(Application.Tests): Add CommentServiceTests for unit testing CommentService.
    Introduce a new `CommentServiceTests` to test the `CommentService` functionality.
    
    - Mock dependencies including `IUnitOfWork`, `IGenericRepository<Comment>`, `IQuestService`, and `IUserQuestService`.
    - Configure AutoMapper with the `CommentProfile` mapping profile.
    - Initialize the `CommentService` instance under test (`_sut`) with mocked dependencies.

---
+++ Application.Tests/Services/CommentServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.MappingProfiles;
+using Application.ServiceAbstractions;
+using Application.Services;
+using AutoMapper;
+using Domain.Contracts;
+using Domain.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+
+namespace Application.Tests.Services;
+
+internal class CommentServiceTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly Mock<IGenericRepository<Comment>> _commentRepositoryMock;
+    private readonly Mock<IQuestService> _questServiceMock;
+    private readonly Mock<IUserQuestService> _userQuestServiceMock;
+    private readonly IMapper _mapper;
+
+    private readonly CommentService _sut;
+    public CommentServiceTests()
+    {
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _commentRepositoryMock = new Mock<IGenericRepository<Comment>>();
+        _unitOfWorkMock.Setup(uow => uow.Repository<Comment>()).Returns(_commentRepositoryMock.Object);
+
+        _questServiceMock = new Mock<IQuestService>();
+        _userQuestServiceMock = new Mock<IUserQuestService>();
+
+        var nullLoggerFactory = new NullLoggerFactory();
+        var config = new MapperConfiguration(cfg => cfg.AddProfile<CommentProfile>(), nullLoggerFactory);
+        _mapper = config.CreateMapper();
+
+        _sut = new CommentService(_unitOfWorkMock.Object, _mapper, _questServiceMock.Object, _userQuestServiceMock.Object);
+    }
+}
