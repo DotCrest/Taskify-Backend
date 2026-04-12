a5f069e78e99b955a944dc5d82d96a6b8b22d7c5
BassimEhab <bassimstudy@gmail.com>
2026-04-12 06:56:51 +0200

    feat(Application): Add IsValidated method and improve code verification logic.
    Updated the `ICodeVerificationService` interface to include the
    `IsValidated` method for checking if a code is validated. Modified
    `CodeVerificationService` to update the `IsVerified` property
    instead of deleting codes upon successful validation. Introduced
    a new `UpdateCode` method to persist changes to the database.
    
    These changes enhance the verification process by allowing
    additional checks before removing validated codes.

--- Application/ServiceAbstractions/ICodeVerificationService.cs
+++ Application/ServiceAbstractions/ICodeVerificationService.cs
@@ -7,4 +7,5 @@ public interface ICodeVerificationService
 {
     Task<BaseToReturnDto> SendCode(string email);
     Task<Result<BaseToReturnDto>> ValidateCode(VerifyCodeDto verifyCodeDto);
+    Task<bool> IsValidated(string email);
 }
